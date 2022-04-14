231be9e206fb79d2707a2ae2ff49d715773bfb80
Rayan Saeed <rayancodes@gmail.com>
2022-04-14 15:18:48 +0500

    Initial commit

---
+++ PixabayApp/PixabayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixabayAppApp.swift
+//  PixabayApp
+//
+//  Created by Rayan Saeed on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PixabayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
