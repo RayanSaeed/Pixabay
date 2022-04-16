c328eff9fc564f54ff18f5cdf28356a12a059336
Rayan Saeed <rayancodes@gmail.com>
2022-04-16 23:44:24 +0500

    Add NetworkWorker

--- PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesNetworkWorker.swift
+++ PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesNetworkWorker.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Combine
+
+final class SearchImagesNetworkWorker: CombineAPI {
+
+	let session: URLSession
+
+	init(configuration: URLSessionConfiguration) {
+		self.session = URLSession(configuration: configuration)
+	}
+
+	convenience init() {
+		self.init(configuration: .default)
+	}
+
+	func getImages(_ requestEndpoint: PixabayImagesEndpoint) -> AnyPublisher<[PixabayImage], Error> {
+		execute(requestEndpoint.request, decodingType: [PixabayImage].self, retries: 2)
+	}
+}
+
+enum PixabayImagesEndpoint {
+	case search(_ keyword: String)
+}
+
+extension PixabayImagesEndpoint: Endpoint {
+
+	var base: String { "https://pixabay.com/api/" }
+
+	var path: String {
+		switch self {
+			case .search(let keyword):
+				return "&q=\(keyword)"
+			// we can add more switch cases here as needed
+			// ...
+		}
+	}
+
+	var apiKey: String {
+		return "api_key=34a92f7d77a168fdcd9a46ee1863edf1"
+	}
+}
