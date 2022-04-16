937362cda4b9bb92c67c056627044b90b3c3bfba
Rayan Saeed <rayancodes@gmail.com>
2022-04-17 00:58:45 +0500

    Update PixabayImage Model

--- PixabayApp/PixabayImages/Models/PixabayImage.swift
+++ PixabayApp/PixabayImages/Models/PixabayImage.swift
@@ -7,6 +7,41 @@
 
 import Foundation
 
+struct PixabayImageSearchResponse: Decodable {
+	let total: Int
+	let totalHits: Int
+	let hits: [PixabayImage]
+}
+
 struct PixabayImage: Codable {
-	
+
+	/// Unique ID of the Pixabay Image
+	let id: Int
+
+	/// Photo type: Possible values = {"photo", "illustration", "vector"}
+	let type: String
+
+	/// Photo tags: e.g "sunflower, flower, plant",
+	let tags: String
+
+	/// The url for webformat image
+	let webformatURL: String
+
+	/// Image dimensions for webformat
+	let webformatWidth, webformatHeight: Int
+
+	/// The url for large size image
+	let largeImageURL: String
+
+	/// Image dimensions for large image
+	let imageWidth, imageHeight: Int
+
+	/// Image download size in bytes
+	let imageSize: Int
+
+	/// Number of views the image has
+	let views: Int
+
+	/// Number of times the image was downloaded
+	let downloads: Int
 }
