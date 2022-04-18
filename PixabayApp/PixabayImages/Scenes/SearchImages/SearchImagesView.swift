dc42595fad5af2a6d53de40a2353441792140018
Rayan Saeed <rayancodes@gmail.com>
2022-04-18 18:23:52 +0500

    Update Search Images View

--- PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesView.swift
+++ PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesView.swift
@@ -13,7 +13,7 @@ protocol SearchImagesDisplayLogic {
 
 extension SearchImagesView: SearchImagesDisplayLogic {
 	func displayImages(viewModel: SearchImages.Search.ViewModel) {
-		// self.images = viewModel.images
+		self.images = viewModel.images
 	}
 
 	func fetchImages(keyword: String) {
@@ -25,15 +25,37 @@ extension SearchImagesView: SearchImagesDisplayLogic {
 struct SearchImagesView: View {
 	var interactor: SearchImagesBusinessLogic?
 
-    var body: some View {
-		VStack {
-			Text("Hello, World!")
+	@State private var searchText: String = ""
+	@State var images: [Image]?
+
+	var body: some View {
+		NavigationView{
+			GeometryReader { geo in
+				ScrollView {
+					LazyVGrid(columns: [
+						GridItem(.flexible()),
+						GridItem(.flexible()),
+						GridItem(.flexible())
+					], spacing: 3) {
+//						ForEach(images, \.self) { image in
+						ForEach(0..<10) { _ in
+							Image(systemName: "circle")
+							.frame(width: geo.size.width / 3, height: geo.size.width / 3)
+							.background(.gray)
+						}
+					}
+				}
+			}
+			.navigationBarTitle(Text("Pixabay Images"), displayMode: .inline)
+
 		}
-    }
+		.searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+	}
 }
 
 struct SearchImagesView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchImagesView()
-    }
+	static var previews: some View {
+		SearchImagesView()
+	}
 }
+
