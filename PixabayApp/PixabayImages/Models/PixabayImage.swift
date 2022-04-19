d07e1ad73abc2e11c40d6f0a8db502779092363c
Rayan Saeed <rayancodes@gmail.com>
2022-04-19 12:30:44 +0500

    Add PixabayImage sampleImage for debugging

--- PixabayApp/PixabayImages/Models/PixabayImage.swift
+++ PixabayApp/PixabayImages/Models/PixabayImage.swift
@@ -45,3 +45,20 @@ struct PixabayImage: Codable {
 	/// Number of times the image was downloaded
 	let downloads: Int
 }
+
+extension PixabayImage {
+	static var sampleImage: PixabayImage { PixabayImage (
+		id: 1236875,
+		type: "photo",
+		tags: "panda, panda bear, sleep",
+		webformatURL: "https://pixabay.com/get/g1ed4a8f7dd774e1512a5f443183bee92bf8a02f511c0a1d525b069f018eea1bb894409c89ae54663140312262d142e770a93d45720f4f07292b9a4497f187d9c_640.jpg",
+		webformatWidth: 640,
+		webformatHeight: 426,
+		largeImageURL: "https://pixabay.com/get/g95cb0048aca5fe02492404c2ebbf9ec2cb34f2ef1343842da44852476bcbb32222a935bb082c1b57d94a48dd6cd2f61a2da78303c8e24666e20a3dd73e496136_1280.jpg",
+		imageWidth: 3456,
+		imageHeight: 2304,
+		imageSize: 1684392,
+		views: 133409,
+		downloads: 71463
+	)}
+}
