08ba73e25895b7877320415e748f9490cc824bb5
Rayan Saeed <rayancodes@gmail.com>
2022-04-19 12:31:09 +0500

    Add SearchImages Router

--- PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesRouter.swift
+++ PixabayApp/PixabayImages/Scenes/SearchImages/SearchImagesRouter.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+protocol SearchImagesRoutingLogic {
+	associatedtype V: View
+	func makeImageDetailsView(with pixabayImage: PixabayImage) -> V
+}
+
+class SearchImagesRouter: SearchImagesRoutingLogic {
+
+	func makeImageDetailsView(with pixabayImage: PixabayImage) -> some View {
+		let detailsView = ImageDetailsView(image: pixabayImage).configureView()
+		return detailsView
+	}
+}
