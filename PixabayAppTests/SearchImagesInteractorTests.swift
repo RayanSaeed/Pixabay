91ca4b8c8d57780c7288e8f5576dd98ba96f0314
Rayan Saeed <rayancodes@gmail.com>
2022-04-20 15:04:39 +0500

    Update failing test to use InteractorSpy to mock the network request

--- PixabayAppTests/SearchImagesInteractorTests.swift
+++ PixabayAppTests/SearchImagesInteractorTests.swift
@@ -9,12 +9,12 @@ import XCTest
 @testable import PixabayApp
 
 class SearchImagesInteractorTests: XCTestCase {
-	var sut: SearchImagesInteractor!
+	var sut: SearchImagesInteractorSpy!
 	var presenterSpy: SearchImagesPresenterSpy!
 
 	override func setUpWithError() throws {
 		try super.setUpWithError()
-		sut = SearchImagesInteractor()
+		sut = SearchImagesInteractorSpy()
 		presenterSpy = SearchImagesPresenterSpy()
 	}
 
@@ -29,6 +29,14 @@ class SearchImagesInteractorTests: XCTestCase {
 		}
 	}
 
+	class SearchImagesInteractorSpy: SearchImagesBusinessLogic {
+		var presenter: SearchImagesPresenterSpy?
+
+		func loadImages(request: SearchImages.Search.Request) {
+			presenter?.presentImagesCalled = true
+		}
+	}
+
 	// MARK: - Tests
 	func testLoadImagesCallsPresenterToPresentImages() {
 		// Given
@@ -39,13 +47,10 @@ class SearchImagesInteractorTests: XCTestCase {
 		sut.loadImages(request: request)
 
 		// Then
-		waitForExpectations(timeout: 5.0) { [weak self] _ in
-			guard let self = self else { return }
-			XCTAssertTrue(
-				self.presenterSpy.presentImagesCalled,
-				"loadImages(request:) should ask the presenter to present the images that it loaded"
-			)
-		}
+		XCTAssertTrue(
+			self.presenterSpy.presentImagesCalled,
+			"loadImages(request:) should ask the presenter to present the images that it loaded"
+		)
 	}
 
 	override func tearDownWithError() throws {
