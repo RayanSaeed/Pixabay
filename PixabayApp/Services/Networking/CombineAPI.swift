366c8a7b3eb4020283f1bcc1e58a01d96e712f4f
Rayan Saeed <rayancodes@gmail.com>
2022-06-06 00:23:08 +0500

    Update CombineAPI

--- PixabayApp/Services/Networking/CombineAPI.swift
+++ PixabayApp/Services/Networking/CombineAPI.swift
@@ -12,7 +12,9 @@ protocol CombineAPI {
 	var session: URLSession { get }
 	func execute<T>(_ request: URLRequest,
 					decodingType: T.Type,
+					qualityOfService: DispatchQoS.QoSClass,
 					queue: DispatchQueue,
+					decoder: JSONDecoder,
 					retries: Int) -> AnyPublisher<T, Error> where T: Decodable
 }
 
@@ -20,17 +22,20 @@ extension CombineAPI {
 
 	func execute<T>(_ request: URLRequest,
 					decodingType: T.Type,
+					qualityOfService: DispatchQoS.QoSClass = .default,
 					queue: DispatchQueue = .main,
+					decoder: JSONDecoder = .init(),
 					retries: Int = 0) -> AnyPublisher<T, Error> where T: Decodable {
 		return session.dataTaskPublisher(for: request)
 			.tryMap {
-				guard let response = $0.response as? HTTPURLResponse, response.statusCode == 200 else {
+				guard let response = $0.response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
 					throw APIError.responseUnsuccessful
 				}
 				return $0.data
 			}
-			.decode(type: T.self, decoder: JSONDecoder())
+			.subscribe(on: DispatchQueue.global(qos: qualityOfService))
 			.receive(on: queue)
+			.decode(type: T.self, decoder: decoder)
 			.retry(retries)
 			.eraseToAnyPublisher()
 	}
